5a68ca55bcbb2755cd12b53da4220f0f215a8a13
Adam <ajsyav@gmail.com>
2016-01-27 15:23:04 -0600

    Create GameSettings.h

---
+++ GameSettings.h
@@ -0,0 +1,16 @@
+
+#ifndef __GameSettings_
+#define __GameSettings_
+#include <string>
+
+using namespace std;
+
+string game_fivecarddraw = "FiveCardDraw";
+string err_noinstance = "Instance not available";
+string err_gamestarted = "Game already started";
+string err_unknowngame = "Unknown game";
+string err_nogameprogress = "No game in progress";
+string err_playeralreadyplaying = "Player is already playing";
+string destructor_message = "Game destructor has been called.";
+
+#endif /* defined(__GameSettings__) */
