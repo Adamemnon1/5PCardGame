23adf0204159c56150552c2e1453d147d737c0df
Adam <ajsyav@gmail.com>
2016-01-27 15:21:19 -0600

    Create FiveCardDrawSettings.h

---
+++ FiveCardDrawSettings.h
@@ -0,0 +1,28 @@
+#ifndef __FiveCardDrawSettings_
+#define __FiveCardDrawSettings_
+#include <string>
+
+using namespace std;
+string yesResponse = "yes";
+string noResponse = "no";
+string playerlabel = "Player: ";
+//JZ: at least camel case these names, please.
+string handlabel = "Hand: ";
+string whichcards = "Which cards would player ";
+string liketodiscard = " like to discard? List the indices of the cards you would like to discard (indices start at 0).";
+string inputlabel = ">> ";
+string usurebro = "Are you sure you do not want to discard anything? Enter 'yes' or 'no' please. ";
+string validindices = "Please enter valid indices from 0 to ";
+string repeatedindex = "Repeated index";
+string waterudoing = "Please enter an integer";
+string winround = " wins this round!";
+string winlabel = "Wins: ";
+string losslabel = "Losses: ";
+string leavegamebro = "Do any players want to LEAVE the game? Enter \"yes\" or \"no\".";
+string whosleaving = "Enter the name of the player that is leaving.";
+string whoutalkingbout = "There is no player with the name ";
+string period = ".";
+string outerspace = " ";
+string whoscoming = "Do any players want to ENTER the game? Enter \"yes\" or \"no\".";
+string whatshisnamebro = "Enter the name of the player that is entering.";
+#endif
