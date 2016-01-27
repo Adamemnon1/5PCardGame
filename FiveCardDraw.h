26e3c04443fbcb98d493a6b3c59354806dac5cdd
Adam <ajsyav@gmail.com>
2016-01-27 15:21:01 -0600

    Create FiveCardDraw.h

---
+++ FiveCardDraw.h
@@ -0,0 +1,39 @@
+/*
+Authors: Carlie Abraham, Brian Lam, Adam Sandor
+Date: 4/5/15
+Lab: Lab 3.
+
+*/
+
+#pragma once
+#include "stdafx.h"
+#include <string>
+#include "Hand.h"
+#include "Game.h"
+#include "Player.h"
+#include <memory>
+#include "Deck.h"
+#include "Card.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <fstream>
+using namespace std;
+
+class FiveCardDraw : public Game {
+
+public:
+	FiveCardDraw();
+	virtual int before_turn(Player & pl); //=0 to make the method pure virtual
+	virtual int turn(Player & pl);
+	virtual int after_turn(Player & pl);
+	virtual int before_round();
+	virtual int round();
+	virtual int after_round();
+
+
+protected:
+	size_t dealerPos;
+	Deck discardDeck;
+
+};
